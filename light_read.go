fc3ba00a7b3775a033601e1846e1ceb478217057
Lex Childs <lexchilds@gmail.com>
2013-09-09 14:07:19 -0500

    added clipboard support

--- light_read.go
+++ light_read.go
@@ -1,52 +1,68 @@
 package main
 
 import (
-  "fmt"
-  "os/exec"
+	"fmt"
 	"github.com/BurntSushi/xgbutil"
 	"github.com/BurntSushi/xgbutil/keybind"
 	"github.com/BurntSushi/xgbutil/xevent"
+	"os/exec"
 )
-
 
 func main() {
 
-  X, _ := xgbutil.NewConn()
+	X, _ := xgbutil.NewConn()
 	keybind.Initialize(X)
+
 	cb1 := keybind.KeyPressFun(
 		func(X *xgbutil.XUtil, e xevent.KeyPressEvent) {
-      readit()
+			read_sel()
 		})
 	cb1.Connect(X, X.RootWin(), "control-z", true)
 
-  fmt.Printf("Listening for Crtl-z ...\n")
+	cb2 := keybind.KeyPressFun(
+		func(X *xgbutil.XUtil, e xevent.KeyPressEvent) {
+			read_clip()
+		})
+	cb2.Connect(X, X.RootWin(), "control-q", true)
+
+	fmt.Printf("Listening for Crtl-z ...(Read Selected Text)\n")
+	fmt.Printf("Listening for Crtl-q ...(Read Clipboard Text)\n")
 	xevent.Main(X)
 
 }
 
-
 var player (exec.Cmd)
 
-func readit( ){
-
-  if( player.Process != nil ){
-    player.Process.Kill()
-    player.Process.Wait()
-  }
-
-  xsel := exec.Command("xsel")
-  festival := exec.Command("text2wave","-o", "/dev/stdout","/dev/stdin" )
-  player = *exec.Command("aplay","/dev/stdin" )
-
-  xsel_pipe,_ := xsel.StdoutPipe()
-  festival.Stdin = xsel_pipe
-  festival_pipe,_ := festival.StdoutPipe()
-  player.Stdin = festival_pipe
-
-  xsel.Start()
-  festival.Start()
-  player.Start()
-
+func read_clip() {
+	if player.Process != nil {
+		player.Process.Kill()
+		player.Process.Wait()
+	}
+	xsel := exec.Command("xclip", "-o")
+	festival := exec.Command("text2wave", "-o", "/dev/stdout", "/dev/stdin")
+	player = *exec.Command("aplay", "/dev/stdin")
+	xsel_pipe, _ := xsel.StdoutPipe()
+	festival.Stdin = xsel_pipe
+	festival_pipe, _ := festival.StdoutPipe()
+	player.Stdin = festival_pipe
+	xsel.Start()
+	festival.Start()
+	player.Start()
 }
 
-
+func read_sel() {
+	if player.Process != nil {
+		player.Process.Kill()
+		player.Process.Wait()
+	}
+	xsel := exec.Command("xsel")
+	festival := exec.Command("text2wave", "-o", "/dev/stdout", "/dev/stdin")
+	player = *exec.Command("aplay", "/dev/stdin")
+	xsel_pipe, _ := xsel.StdoutPipe()
+	festival.Stdin = xsel_pipe
+	festival_pipe, _ := festival.StdoutPipe()
+	player.Stdin = festival_pipe
+	xsel.Start()
+	festival.Start()
+	player.Start()
+}
